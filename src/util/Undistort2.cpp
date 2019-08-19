21c05e39fabd7c284d78ec4473b73cd2c2444508
Ahmed Abdel Aal <eng.ahmedhussein@gmail.com>
2019-08-19 22:33:35 +0200

    Add Undistort2

---
+++ src/util/Undistort2.cpp
@@ -0,0 +1,11 @@
+// Internal
+#include "util/Undistort2.hpp"
+
+namespace dso {
+
+Undistort2::Undistort2() = default;
+
+Undistort2::~Undistort2() = default;
+
+} // namespace dso
+
---
+++ src/util/Undistort2.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace dso {
+
+class Undistort2 {
+public:
+  Undistort2();
+
+  ~Undistort2();
+
+};
+
+} // namespace dso
+
